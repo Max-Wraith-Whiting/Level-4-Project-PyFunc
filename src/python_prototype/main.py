35d221725666e12e569e783d2dde5128cf7fb0e3
Nexus <Max.Wraith-Whiting@protonmail.com>
2023-10-02 20:20:58 +0100

    Added basic command argument logic [Undefined Function!]

--- src/python_prototype/main.py
+++ src/python_prototype/main.py
@@ -1,7 +1,14 @@
+import sys
 
 # This is the main entry point for the python prototype
 def main():
-    pass
+    if len(sys.argv) > 1:
+        print("Usage: main.py [script file]")
+    elif len(sys.arv) is 1:
+        runFile(sys.argv[1])
+    else:
+        # A REPL can be called here later.
+        print("Usage: main.py [script file]")
 
 if __name__ == "__main__":
     main()
