a3f13fd509105a7eced5cfbd941556f3078b7be0
Nexus <Max.Wraith-Whiting@protonmail.com>
2023-10-02 20:27:34 +0100

    Added runFile to handle file access and reading

--- src/python_prototype/main.py
+++ src/python_prototype/main.py
@@ -1,4 +1,12 @@
 import sys
+from pathlib import Path
+
+def runFile(pathString):
+    try:
+        with Path(pathString).open() as file:
+            run(file) # Run the whole file in totality.
+    except e:
+        print(e)
 
 # This is the main entry point for the python prototype
 def main():
