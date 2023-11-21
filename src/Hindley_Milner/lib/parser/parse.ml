f29137adfb16ab77206f69972adfa731a9afacba
STI4OMQS <Max.Wraith-Whiting@protonmail.com>
2023-11-21 16:23:40 +0000

    Added parse functions

--- src/Hindley_Milner/lib/parser/parse.ml
+++ src/Hindley_Milner/lib/parser/parse.ml
@@ -2,13 +2,22 @@
 
 open Lexer
 open Lexing
+open Slambda.Errors
+
+let print_position ppf lexbuf = 
+  let pos = lexbuf.lex_curr_p in 
+  Format.fprintf ppf "%s:%d:%d" pos.pos_fname pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
 
 let parse_with_error lexbuf = 
-
+  try Parser.expr_main Lexer.token lexbuf with
+  | Lexical_error msg -> 
+    let msg = Format.asprintf "%a: %s" print_position lexbuf msg in raise (parse_error msg)
+  | Parser.Error ->
+    let msg = Format.asprintf "%a: syntax error" print_position lexbuf in raise (parse_error msg)
+  
 let parse_string str = 
   let lexbuf = Lexing.from_string str in
-  lexbuf.lex_curr_p <- {lexbuf.lex_curr_p with pos_fname = "<string>"};
-  let expr = parse_with_error lexbuf in 
-    expr
+    lexbuf.lex_curr_p <- {lexbuf.lex_curr_p with pos_fname = "<string>"};
+  let expr = parse_with_error lexbuf in expr
 
 (* let parse_with_error lexbuf = try  *)
