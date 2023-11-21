35f03a4cff0127f59dd546f3d20984d608ccaa1c
STI4OMQS <Max.Wraith-Whiting@protonmail.com>
2023-11-21 16:25:26 +0000

    [WIP] Refactored REPL into working ver.

--- src/Hindley_Milner/bin/main.ml
+++ src/Hindley_Milner/bin/main.ml
@@ -1,16 +1,34 @@
 (* Start REPL *)
+open Pyfunc_frontend
+open Slambda.Typechecker
+open Slambda.Errors
+open Slambda.Types
 
-let rec repl() = 
+let pipe str = 
+  let expr = Parse.parse_string str in Typecheck.typecheck expr
+
+let rec repl () = 
   print_string "> ";
   let user_input = read_line() in
-  if user_input = "" then print_string "--- Closing REPL ---\n"
-  else repl();;
+  (* if user_input = "" then print_string "--- Closing REPL ---\n" *)
+  (* else  *)
+  let () = 
+    try
+      let typ = pipe user_input in
+      Format.printf "Type: %a\n" Type.pp typ
+    with 
+    | Parse_Error err -> Format.printf "[Parse Error] %S\n" err
+    | Type_Error err ->  Format.printf "[Type Error] %s\n" err
+    in 
+    let () = Format.print_flush () in
+      repl ();;
 
 
-(* repl();; *)
-
+repl ();;
+(* 
 let expr_main = 
   let open Lexing in
   let lexbuf = Lexing.from_channel stdin in 
-  let result = Pyfunc_frontend.Parse.main Lexer.token lexbuf in 
-  Printf.printf "Result: %d\n" result
+  let result = Parse.parse_string Lexer.token lexbuf in 
+  Printf.printf "Result: %d\n" result *)
+
